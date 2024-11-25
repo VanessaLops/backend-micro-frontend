0f5b2e8534c3badc2e42428cce8a62ac2ae5744c
VanessaLops <vanessashirlene_estrela@hotmail.com>
2024-11-25 16:03:50 -0300

    adicionando cors

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
+
+
+app.use(cors());
 
 const targetUrl = 'https://boasorte.teddybackoffice.com.br/users';
 
 app.use(express.json());
-
 app.use('/api', createProxyMiddleware({
   target: targetUrl,
   changeOrigin: true,
   pathRewrite: {
-    '^/api': '', 
-  }
+    '^/api': '',
+  },
 }));
 
 module.exports = (req, res) => {
