488a2410855aeff20d11bed57f2766f67244be64
VanessaLops <vanessashirlene_estrela@hotmail.com>
2024-11-25 16:12:54 -0300

    adicionando os metodos HTTP

--- server.js
+++ server.js
@@ -4,16 +4,21 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'] 
+}));
 
 const targetUrl = 'https://boasorte.teddybackoffice.com.br/users';
 
 app.use(express.json());
+
 app.use('/api', createProxyMiddleware({
   target: targetUrl,
   changeOrigin: true,
   pathRewrite: {
-    '^/api': '',
+    '^/api': '', // Remove '/api' do caminho da URL antes de encaminhar para o targetUrl
   },
 }));
 
